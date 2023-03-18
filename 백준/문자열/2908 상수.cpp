4dc6429ef9c99fed78397b6cabda653119db731e
JUNSUNG06 <forest2847@gmail.com>
2023-03-18 13:14:37 +0900

    03.17

---
+++ 백준/문자열/2908 상수.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<string>
+//#include<cmath>
+//using namespace std;
+//
+//int main()
+//{
+//	string str[2];
+//	int num[2] = { 0, 0 };
+//
+//	cin >> str[0] >> str[1];
+//
+//	for (int i = 0; i < 2; i++)
+//	{
+//		for (int j = 2; j >= 0; j--)
+//		{
+//			 num[i] += (str[i][j] - 48) * pow(10, j);
+//		}
+//	}
+//
+//	if (num[0] >= num[1])
+//		cout << num[0];
+//	else
+//		cout << num[1];
+//}
---
+++ 백준/문자열/5622  다이얼.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string str;
+//	int time = 0;
+//
+//	cin >> str;
+//
+//	for (int i = 0; i < str.size(); i++)
+//	{
+//		if (str[i] >= 80)
+//		{
+//			if (str[i] == 'W')
+//			{
+//				time += 10;
+//			}
+//			else
+//			{
+//				time += (str[i] - 80) / 4 + 8;
+//			}
+//		}
+//		else
+//		{
+//			time += (str[i] - 65) / 3 + 3;
+//		}
+//	}
+//
+//	cout << time;
+//}
