4886192730520203e020ecd5f63d98919cc04a97
JUNSUNG06 <101687101+JUNSUNG06@users.noreply.github.com>
2023-05-22 21:02:15 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int cnt;
+    
+    cin >> cnt;
+    
+    for(int i = cnt; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << '*';
+        }
+        
+        cout << endl;
+    }
+}
