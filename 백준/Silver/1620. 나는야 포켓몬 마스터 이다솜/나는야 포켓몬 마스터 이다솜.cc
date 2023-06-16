8f61ba45c963ff39994048a9a47f5f4cbd8ce4b1
JUNSUNG06 <101687101+JUNSUNG06@users.noreply.github.com>
2023-06-16 10:38:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 228 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    map<int, string> iMap;
+    map<string, int> sMap;
+    int n, m;
+    string str;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+        iMap.insert(make_pair(i + 1, str));
+        sMap.insert(make_pair(str, i + 1));
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> str;
+
+        if (sMap.find(str) != sMap.end())
+        {
+            cout << sMap[str] << '\n';
+            continue;
+        }
+        
+        cout << iMap[stoi(str)] << '\n';
+    }
+}
