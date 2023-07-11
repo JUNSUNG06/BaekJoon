c7828897f7f91965d1653e68b66f9f9557e72936
JUNSUNG06 <101687101+JUNSUNG06@users.noreply.github.com>
2023-07-11 16:25:35 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,56 +1,10 @@
-#include <iostream>
-#include <stack>
-using namespace std;
-
-int main()
-{
-	int raserCount = 0;
-	int stickCount = 0;
-
-	int result = 0;
-
-	string obj;
-	stack<char> objInfo;
-	char beforeChar;
-	cin >> obj;
-
-	for (int i = 0; i < obj.size(); i++)
-	{
-		if (!objInfo.empty())
-		{
-			beforeChar = obj[i - 1];
-		}
-
-		if (obj[i] == '(')
-		{
-			objInfo.push(obj[i]);
-			stickCount++;
-			
-		}
-		else
-		{
-			if (beforeChar == '(')
-			{
-				raserCount++;
-				stickCount--;
-				
-				result += stickCount;
-			}
-			else
-			{
-				stickCount--;
-				result++;
-			}
-
-			objInfo.pop();
-		}
-
-		if (objInfo.empty())
-		{
-			raserCount = 0;
-			stickCount = 0;
-		}
+#include <cstdio>
+int cnt = 0, ans = 0, ch, pch;
+int main() {
+	while ((ch=getchar()) != '\n') {
+		if (ch == '(') cnt++;
+		else cnt--, ans += pch == '(' ? cnt : 1;
+		pch = ch;
 	}
-
-	cout << result;
+	printf("%d", ans);
 }
