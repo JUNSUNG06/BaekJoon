2e36c8eacdba2e8f50d2ef91c0f48fda6a33f351
JUNSUNG06 <101687101+JUNSUNG06@users.noreply.github.com>
2023-11-05 23:47:57 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -3,9 +3,8 @@
 #include<vector>
 using namespace std;
 
-vector<int> arr = {};
+vector<int> arr;
 
-//내림차순
 void quickSort(int start, int end)
 {
 	//정렬할게 하나 이하면 안 함
@@ -16,13 +15,10 @@ void quickSort(int start, int end)
 	int right = end;
 	int temp = 0;
 
-	//아래 작업 교차할 때 까지 반복
 	while (true)
 	{
-		//피봇보다 크거나 오른쪽 인덱스보다 작을 때 만
 		while (arr[left] >= arr[start] && left < end)
 			left++;
-		//피봇보다 작거나 왼족 인덱스보다 크거나 같을 때 만
 		while (arr[right] <= arr[start] && right > start)
 			right--;
 
@@ -38,32 +34,30 @@ void quickSort(int start, int end)
 		}
 	}
 
-	//가장 오른쪽에 있는 피봇보다 작은 값과 피봇 변경
 	temp = arr[start];
 	arr[start] = arr[right];
 	arr[right] = temp;
 
-	//왼쪽, 오른쪽 정렬
 	quickSort(start, right - 1);
 	quickSort(right + 1, end);
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	
-	string str;
-	cin >> str;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < str.size(); i++)
-	{
-		arr.push_back(str[i] - 48);
-	}
+    string num;
+    cin >> num;
 
-	quickSort(0, str.size() - 1);
+    for (int i = 0; i < num.size(); i++)
+    {
+        arr.push_back(num[i] - 48);
+    }
 
-	for (int i = 0; i < str.size(); i++)
-		cout << arr[i];
+    quickSort(0, num.size() - 1);
+
+    for (int i = 0; i < num.size(); i++)
+        cout << arr[i];
 }
